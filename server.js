28b47d85352257487ac3cf4eca2d6f0ce694e5c3
HursitTarcan <hursittarcan@gmail.com>
2022-06-15 16:36:42 +0200

    test

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) =>
+{
+    res.statusCode = 200;
+    res.end('Hello World,ZEET');
+});
+
+server.listen(port, hostname, () => {
+    console.log(
+        "Server running at")
+});
